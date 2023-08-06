82a633b067f8c79c573e690db995d4371ae3b4f5
Ervin-Capuno <ervinreycapuno23@gmail.com>
2023-08-06 03:47:50 -0400

    Created the main section and fix issues when the content is not showing

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const toggleButton = document.getElementById("toggleButton");
 const sidebar = document.getElementById("sidebar");
 const navLinks = document.querySelectorAll(".nav-list a");
+const mainContent = document.querySelector(".main-content");
 
 toggleButton.addEventListener("click", function() {
     sidebar.classList.toggle("active");
     toggleButton.classList.toggle("active");
+    mainContent.classList.toggle("slide-down", sidebar.classList.contains("active"));
 });
 
-// Add event listener to each navigation link
 navLinks.forEach(link => {
     link.addEventListener("click", function() {
         sidebar.classList.remove("active");
         toggleButton.classList.remove("active");
+        mainContent.classList.remove("slide-down");
     });
 });
 
