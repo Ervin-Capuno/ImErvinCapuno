f3ac6bd0100388505ba5f6d2a285336939b6c150
Ervin Capuno <21-00082@g.batstate-u.edu.ph>
2023-08-28 07:49:53 +0800

    fix the bugs

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 const toggleButton = document.getElementById("toggleButton");
 const sidebar = document.getElementById("sidebar");
 const navLinks = document.querySelectorAll(".nav-list a");
-const mainContent = document.querySelector(".main-content");
+
 
 toggleButton.addEventListener("click", function() {
     sidebar.classList.toggle("active");
     toggleButton.classList.toggle("active");
-    mainContent.classList.toggle("slide-down", sidebar.classList.contains("active"));
+
 });
 
 navLinks.forEach(link => {
     link.addEventListener("click", function() {
         sidebar.classList.remove("active");
         toggleButton.classList.remove("active");
-        mainContent.classList.remove("slide-down");
+
     });
 });
+
+
